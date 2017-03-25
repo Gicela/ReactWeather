7f14799def79c60d50985ab86708a3c8da44524a
Gicela <gicela@mexicanwave.comq>
2017-03-25 15:29:29 +0000

    weather app starting components

---
+++ app/components/Examples.jsx
@@ -0,0 +1,13 @@
+var React = require("react");
+
+var Examples = React.createClass({
+    render: function(){
+        return(
+            <div>
+                <h2>Examples component</h2>
+                </div>
+        );
+    }
+});
+
+module.exports = Examples;
---
+++ app/components/Weather.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var Weather = React.createClass({
+    render: function(){
+        return (
+            <h2>Weather component</h2>
+        )
+    }
+});
+
+module.exports = Weather;
