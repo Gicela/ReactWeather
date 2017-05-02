202d9440e61bfcd0500bdf5bb43695711dcc0e8b
Gicela <gicela@mexicanwave.comq>
2017-05-02 15:32:10 +0100

    New paragraph

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,8 +2,10 @@ var React = require('react');
 
 var About = (props) => {
         return(
+            <div>
             <h2>About page component</h2>
             <p>Welcome to the About page</p>
+            </div>
         );
 }
 module.exports = About;
